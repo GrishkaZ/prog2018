6c63c3f8dbe693b79cdf58967a96c34ca6048f86
GrishkaZ <36984978+GrishkaZ@users.noreply.github.com>
2018-03-14 22:17:39 +0500

    Быстрая сортировка, лаба 3

---
+++ ConsoleApplication9/ConsoleApplication9/QuickSort.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace QuickSort
+{
+    public class QuickSorting
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[0];
+            QuickSort(array, 0, array.Length - 1);
+            Console.ReadKey();
+        }
+
+        public static void QuickSort(int[] array, int first, int last)
+        {
+            if (array.Length != 0)
+            {
+                int p = array[(last - first) / 2 + first];
+                int temp;
+                int i = first, j = last;
+                while (i <= j)
+                {
+                    while (array[i] < p && i <= last) ++i;
+                    while (array[j] > p && j >= first) --j;
+                    if (i <= j)
+                    {
+                        temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                        ++i; --j;
+                    }
+                }
+                if (j > first) QuickSort(array, first, j);
+                if (i < last) QuickSort(array, i, last);
+            }
+            else Console.WriteLine("массив пустой");
+        }
+    }
+}
+
---
+++ ConsoleApplication9/UnitTestProject1/QuickSortTest.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QuickSort;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class QuickSortTest
+    {
+        [TestMethod]
+        public void TestLitleArray()
+        {
+            bool test = true;
+            int[] array = new[] { 4, 8, 3 };           
+            QuickSorting.QuickSort(array, 0, array.Length - 1);
+            if (array[0] > array[1] || array[1] > array[2])
+                test = false;
+            Assert.IsTrue(test);
+        }
+
+        [TestMethod]
+        public void TestSameNumberArray()
+        {
+            bool test = true;
+            int[] array = new int [100];
+            for (int i = 0; i < array.Length; i++)
+                array[i] = 1;
+            QuickSorting.QuickSort(array, 0, array.Length - 1);
+            if (array[0] > array[array.Length - 1])
+                test = false;
+            Assert.IsTrue(test);
+        }
+
+        [TestMethod]
+        public void TestRandomArray()
+        {
+            bool test = true;
+            int[] array = new int[1000];
+            var rand = new Random();
+            for (int i = 0; i < array.Length; i++)
+                array[i] = rand.Next(0, 999);
+            QuickSorting.QuickSort(array, 0, array.Length - 1);
+            /* полная проверка всего массива
+             for (int i = 0; i < array.Length - 1; i++)
+            {
+                if (array[i] > array[i + 1])
+                {
+                    test = false;
+                    break;
+                }
+            }*/
+            for (int i = 0; i < 10; i++)
+            {
+                var a = rand.Next(0, 998);
+                if (array[a] > array[a + 1])
+                {
+                    test = false;
+                    break;
+                }
+            }
+            Assert.IsTrue(test);
+        }
+
+        [TestMethod]
+        public void TestEmptyArray()
+        {
+            bool test = true;
+            int[] array = new int[0];           
+            QuickSorting.QuickSort(array, 0, array.Length - 1);
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                if (array[i] > array[i + 1])
+                {
+                    test = false;
+                    break;
+                }
+            }
+            Assert.IsTrue(test);
+        }
+
+        [TestMethod]
+        public void TestBigArray()
+        {
+            bool test = true;
+            int[] array = new int[150000000]; // 4гб оперативной памяти, процессор i3
+            var rand = new Random();
+            for (int i = 0; i < array.Length; i++)
+                array[i] = rand.Next(0, 1499999);
+            QuickSorting.QuickSort(array, 0, array.Length - 1);           
+             for (int i = 0; i < array.Length - 1; i++)
+            {
+                if (array[i] > array[i + 1])
+                {
+                    test = false;
+                    break;
+                }
+            }           
+            Assert.IsTrue(test);
+        }
+    }
+}
