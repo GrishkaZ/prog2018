903fc7b1d3d11139df6d737ef55393c3b5d2c079
GrishkaZ <36984978+GrishkaZ@users.noreply.github.com>
2018-04-07 00:05:05 +0500

    Лаба 4
    Хэш-таблица

---
+++ HeshTable/HeshTable/HashTable.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HeshTable
+{ 
+    public class HashTable
+    {
+         class Pair
+        {
+            /// <summary>
+            /// Ключ
+            /// </summary>
+            public object Key { get; set; }
+            /// <summary>
+            /// Значение
+            /// </summary>
+            public object Value { get; set; }
+        }
+        List<List<Pair>> list;
+        /// <summary>
+        /// Конструктор контейнера
+        /// summary>
+        /// size">Размер хэ-таблицы
+        public HashTable(int size)
+        {
+            list = new List<List<Pair>>();
+            for(int i =0; i<size; i++)
+            {
+                list.Add(new List<Pair>());
+            }               
+        }
+        /// 
+        /// Метод складывающий пару ключь-значение в таблицу
+        /// 
+        /// key">
+        /// value">
+        public  void PutPair(object key, object value)
+        {
+            var bucketNumber = GetBucketNumber(key);
+            foreach (var el in list[bucketNumber])
+            {
+                if(Equals(el.Key, key))
+                {
+                    el.Value = value;
+                    return;
+                }
+            }
+            list[bucketNumber].Add(new Pair { Key = key, Value = value });
+        }
+        /// <summary>
+        /// Поиск значения по ключу
+        /// summary>
+        /// key">Ключь
+        /// <returns>Значение, null если ключ отсутствуетreturns>
+        public  object GetValueByKey(object key)
+        {
+            var bucketNumber = GetBucketNumber(key);
+            foreach (var el in list[bucketNumber])
+            {
+                if (Equals(el.Key, key))
+                {  
+                    return el.Value;
+                }
+            }
+            return null;
+        }
+        int GetBucketNumber(object key)
+        {
+            return Math.Abs(key.GetHashCode()) % list.Count;
+        }     
+    }
+}
---
+++ HeshTable/HeshTableTest/HashTableTest1.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HeshTable;
+
+namespace HashTableTest
+{
+    [TestClass]
+    public class HashTableTest1
+    {
+        [TestMethod]
+        public void TestThreeElements()
+        {
+            string[] value = new string[] { "один", "два", "три" };
+            var table = new HashTable(3);
+            table.PutPair(1, value[0]);
+            table.PutPair(2, value[1]);
+            table.PutPair(3, value[2]);
+            Assert.AreEqual(table.GetValueByKey(1), value[0]);
+            Assert.AreEqual(table.GetValueByKey(2), value[1]);
+            Assert.AreEqual(table.GetValueByKey(3), value[2]);
+
+        }
+        [TestMethod]
+        public void Test2EqualElements()
+        {
+            string[] value = new string[] { "один", "два" };
+            var table = new HashTable(3);
+            table.PutPair(1, value[0]);
+            table.PutPair(1, value[1]);
+            Assert.AreEqual(table.GetValueByKey(1), value[1]);
+        }
+
+        [TestMethod]
+        public void TestManyElements()
+        {
+            int size = 100000;
+            var table = new HashTable(size);
+            for (int i = 0; i < size; i++)
+                table.PutPair(i, i + 1);
+            Assert.AreEqual(table.GetValueByKey(70000), 70001);
+        }
+
+
+        [TestMethod]
+        public void TestManySearchlElements()
+        {
+            int size = 100;
+            var table = new HashTable(size);
+            for (int i = 0; i < size; i++)
+                table.PutPair(i, i + 1);
+            for (int i = 0; i < size; i++)
+            {
+                Assert.AreEqual(table.GetValueByKey(i), i + 1);
+            }
+        }
+    }
+}
+
